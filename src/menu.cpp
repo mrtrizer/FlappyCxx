403aeec82678e4b531d19e7ec356e00a208ea94d
trizer <mrtrizer@gmail.com>
2016-02-26 12:58:01 +0300

    Removed extra sprites.

--- src/menu.cpp
+++ src/menu.cpp
@@ -13,7 +13,7 @@ void Menu::init() {
     setActiveCamera(getRoot()->ADD_CHILD(GObjCamera,100,1,500,POS(0,0,0)));
     getRoot()->ADD_CHILD(GDecor,"logo",40,10,POS(-20,30,1));
     getRoot()->ADD_CHILD(GDecor,"background",200,200,POS(-100,-100,0));
-    getRoot()->ADD_CHILD(GAnimation,"bird_anim", 8, 10, POS(-4, 20 -4, 20),2,0.5);
+    getRoot()->ADD_CHILD(GAnimation,"bird", 8, 10, POS(-4, 20 -4, 20),2,0.5);
     scorePanel = getRoot()->ADD_CHILD(Score,POS(-6.5, 10 -4, 5));
     getRoot()->ADD_CHILD(Button,[this](){
         flappyCtrl.putSymbol(Ctrl::START);
